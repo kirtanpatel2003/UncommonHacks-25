39249fb2c82d4349f8e7db14b34c3c1059a3a49b
Kirtan Patel <kpate452@uic.edu>
2025-03-30 04:53:02 -0500

    name refactored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./components/home"
+// import Home from "./components/home"
+import Home from "./components/Home"
 import WeatherLandingPage from "./components/landingPage"
 
 function App() {
